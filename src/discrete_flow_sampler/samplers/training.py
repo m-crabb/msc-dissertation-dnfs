d8bdc5a66cce6b3b28ef4d1c13aa12806e5e2e68
Mitchell Crabb <mitchcrabb@tutamail.com>
2026-05-08 20:20:12 +0100

    feat: clip gradient norm at 1.0

--- src/discrete_flow_sampler/samplers/training.py
+++ src/discrete_flow_sampler/samplers/training.py
@@ -162,6 +162,7 @@ def train(
                 )
                 optimiser.zero_grad()
                 loss_value.backward()
+                torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
                 optimiser.step()
 
                 wall_clock_step_s = time.time() - step_start
